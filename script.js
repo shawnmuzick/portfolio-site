abb6c778c677b6a053ce1d100b1b3f24c4217d1a
Shawn Matyasovszky <shawnmuzick@gmail.com>
2020-07-14 00:24:17 -0400

    switch from mobile nav to full nav with size

--- script.js
+++ script.js
@@ -1,17 +1,32 @@
-const nav_expand_btn = document.getElementById("mobile-nav-expand");
-const nav_menu = document.getElementById("nav-menu");
-const nav_links = nav_menu.querySelectorAll("li.landing-menu-item");
+const nav_expand_btn = document.getElementById('mobile-nav-expand');
+const nav_menu = document.getElementById('nav-menu');
+const nav_links = nav_menu.querySelectorAll('li.landing-menu-item');
+const form_contact_submit = document.getElementById('form_contact_submit');
+const contact_name = document.getElementById('contact_name');
+const contact_email = document.getElementById('contact_email');
+const contact_organization = document.getElementById('contact_organization');
+const contact_message = document.getElementById('contact_message');
 
 function handle_nav() {
-    if (nav_menu.classList.contains("show")) {
-        nav_menu.classList.remove("show");
-    } else {
-        nav_menu.classList.add("show");
-    }
+	if (nav_menu.classList.contains('show')) {
+		nav_menu.classList.remove('show');
+	} else {
+		nav_menu.classList.add('show');
+	}
 }
 
-nav_expand_btn.addEventListener("click", handle_nav);
+nav_expand_btn.addEventListener('click', handle_nav);
 
 nav_links.forEach((link) => {
-    link.addEventListener("click", handle_nav);
+	link.addEventListener('click', handle_nav);
+});
+form_contact_submit.addEventListener('click', (e) => {
+	e.preventDefault();
+	const form_data = {
+		name: contact_name.value,
+		email: contact_email.value,
+		organization: contact_organization.value,
+		message: contact_message.value,
+	};
+	console.log(form_data);
 });
