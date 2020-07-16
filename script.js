db9a3f0b2d19c4467dc75f5204949cb7f4b9955e
Shawn Matyasovszky <shawnmuzick@gmail.com>
2020-07-16 00:29:14 -0400

    added firebase, connected form to database

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
+import { firebaseConfig } from './firebase_config.js';
 const nav_expand_btn = document.getElementById('mobile-nav-expand');
 const nav_menu = document.getElementById('nav-menu');
 const nav_links = nav_menu.querySelectorAll('li.landing-menu-item');
+const form_contact = document.getElementById("form_contact");
 const form_contact_submit = document.getElementById('form_contact_submit');
 const contact_name = document.getElementById('contact_name');
 const contact_email = document.getElementById('contact_email');
 const contact_organization = document.getElementById('contact_organization');
 const contact_message = document.getElementById('contact_message');
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+const firestore = firebase.firestore();
+const db = firestore.collection('contactData');
 
 function handle_nav() {
 	if (nav_menu.classList.contains('show')) {
@@ -28,5 +35,12 @@ form_contact_submit.addEventListener('click', (e) => {
 		organization: contact_organization.value,
 		message: contact_message.value,
 	};
-	console.log(form_data);
-});
+	db.doc()
+		.set(form_data)
+		.then(() => {
+			console.log('data saved!');
+			window.alert('Message recieved!');
+		})
+		.catch((err) => console.log(err));
+		form_contact.reset();
+	});
