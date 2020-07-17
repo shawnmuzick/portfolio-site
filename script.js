5beb73ed5ef96f656ccdee73891d2e1c70b55638
Shawn Matyasovszky <shawnmuzick@gmail.com>
2020-07-16 23:59:17 -0400

    added form recaptcha and error fields

--- script.js
+++ script.js
@@ -2,18 +2,28 @@ import { firebaseConfig } from './firebase_config.js';
 const nav_expand_btn = document.getElementById('mobile-nav-expand');
 const nav_menu = document.getElementById('nav-menu');
 const nav_links = nav_menu.querySelectorAll('li.landing-menu-item');
-const form_contact = document.getElementById("form_contact");
+const form_contact = document.getElementById('form_contact');
 const form_contact_submit = document.getElementById('form_contact_submit');
 const contact_name = document.getElementById('contact_name');
 const contact_email = document.getElementById('contact_email');
 const contact_organization = document.getElementById('contact_organization');
 const contact_message = document.getElementById('contact_message');
+const captcha = document.getElementById('recaptcha-container');
+const form_contact_error = document.getElementById('form_contact_error');
+let CAPTCHA_VALIDATED = false;
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
+firebase.auth();
 const firestore = firebase.firestore();
 const db = firestore.collection('contactData');
 
+window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier(captcha, {
+	callback: function (response) {
+		CAPTCHA_VALIDATED = true;
+	},
+});
+recaptchaVerifier.render();
 function handle_nav() {
 	if (nav_menu.classList.contains('show')) {
 		nav_menu.classList.remove('show');
@@ -27,8 +37,28 @@ nav_expand_btn.addEventListener('click', handle_nav);
 nav_links.forEach((link) => {
 	link.addEventListener('click', handle_nav);
 });
+const form_validate = () => {
+	if (!form_contact.checkValidity()) {
+		form_contact_error.style.display = 'block';
+		form_contact_error.innerText = 'Please fill out all required fields!';
+		return;
+	} else {
+		return true;
+	}
+};
+const form_reset = () => {
+	form_contact.reset();
+	form_contact_error.style.display = 'none';
+	form_contact_error.innerText = '';
+};
 form_contact_submit.addEventListener('click', (e) => {
 	e.preventDefault();
+	if (!form_validate()) return;
+	if (!CAPTCHA_VALIDATED) {
+		form_contact_error.style.display = 'block';
+		form_contact_error.innerText = 'Please complete the captcha!';
+		return;
+	}
 	const form_data = {
 		name: contact_name.value,
 		email: contact_email.value,
@@ -42,5 +72,5 @@ form_contact_submit.addEventListener('click', (e) => {
 			window.alert('Message recieved!');
 		})
 		.catch((err) => console.log(err));
-		form_contact.reset();
-	});
+	form_reset();
+});
